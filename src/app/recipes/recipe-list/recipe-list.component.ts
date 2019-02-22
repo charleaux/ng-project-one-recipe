154b48197a35c3a196108902878ff1055a408f13
miamidade\e314637 <charles.torry@miamidade.gov>
2019-02-22 17:00:35 -0500

    changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
@@ -16,10 +17,13 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'A test recipe2',
       'This is a test2',
-      'https://pixnio.com/free-images/2017/09/21/2017-09-21-06-45-25-1100x723.jpg'
+      'https://www.maxpixel.net/static/photo/1x/Food-Kitchen-Meals-Home-Made-Dishes-Recipe-Bio-1175493.jpg'
     )
   ];
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
   constructor() {}
 
   ngOnInit() {}
